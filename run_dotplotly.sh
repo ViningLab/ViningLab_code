8841cbc4ee0686128e6b7d89a9bda9e13a3a1827
Brian Knaus <briank.lists@gmail.com>
2022-09-09 09:08:40 -0700

    Create run_dotplotly.sh

---
+++ run_dotplotly.sh
@@ -0,0 +1,35 @@
+
+# https://bioinformaticsworkbook.org/dataWrangling/genome-dotplots.html#gsc.tab=0
+# https://github.com/tpoorten/dotPlotly
+
+
+#~/gits/dotPlotly/pafCoordsDotPlotly.R \
+#   -i CBDRX_ERBxHO40_23_dovetail_hap1.paf \
+#   -o CBDRX_ERBxHO40_23_dovetail_hap1.plot \
+#   -m 2000 \
+#   -q 500000 \
+#   -k 10 \
+#   -x \
+#   -s -t -l -p 12
+
+~/gits/dotPlotly/pafCoordsDotPlotly.R \
+   -i CBDRX_Cannbio-2.paf \
+   -o CBDRX_Cannbio-2.plot \
+   -m 2000 \
+   -q 500000 \
+   -k 10 \
+   -x \
+   -s -t -l -p 12
+
+
+#~/gits/dotPlotly/pafCoordsDotPlotly.R \
+#   -i mm2_CBDRx_ERBxHO40_23_COMBINED-Hap2.paf \
+#   -o CBDRx_ERBxHO40_23_COMBINED-Hap2_ref.plot \
+#   -m 2000 \
+#   -q 500000 \
+#   -k 10 \
+#   -r NC_044372.1 \
+#   -r NC_044375.1,NC_044372.1,NC_044373.1 \
+#   -s -t -l -p 12
+
+# EOF
