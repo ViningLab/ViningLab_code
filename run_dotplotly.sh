9f6a352d8f1b37b3d73741110d353fa5309daffa
Brian Knaus <briank.lists@gmail.com>
2022-09-21 17:26:14 -0700

    Update run_dotplotly.sh
    Documented parameters

--- run_dotplotly.sh
+++ run_dotplotly.sh
@@ -2,6 +2,19 @@
 # https://bioinformaticsworkbook.org/dataWrangling/genome-dotplots.html#gsc.tab=0
 # https://github.com/tpoorten/dotPlotly
 
+# -i input filename
+# -o output filename
+# -v verbose
+# -q min-query-length
+# -m min-alignment-length
+# -p plot size in inches
+# -l show horizontal lines
+# -k number-ref-chromosomes
+# -s turn on color alignments by % identity
+# -t turn on calculation of % identity for on-target alignments only
+# -x turn off production of interactive plotly
+# -r comma-separated list of reference IDs to keep
+
 
 #~/gits/dotPlotly/pafCoordsDotPlotly.R \
 #   -i CBDRX_ERBxHO40_23_dovetail_hap1.paf \
