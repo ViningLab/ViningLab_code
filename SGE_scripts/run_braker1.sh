a7c34dbea7c4807e8940da51bcffc244becd80bf
Brian Knaus <briank.lists@gmail.com>
2023-11-08 12:13:19 -0800

    Update run_braker1.sh

--- SGE_scripts/run_braker1.sh
+++ SGE_scripts/run_braker1.sh
@@ -39,24 +39,9 @@ BRAKER="/local/cluster/BRAKER-2.1.5/scripts/braker.pl"
 braker.pl -version
 
 # Ideally a "soft masked" genome.
-#GENOME="~/Vining_Lab_nfs3/GENOMES/hemp/CBDRx/GCF_900626175/RepeatModeler/GCF_900626175.2_cs10_genomic.fna.masked"
-#GENOME="../../CBDRx_hisat2/GCF_900626175.2_cs10_genomic.fna.masked"
-#GENOME="GCF_900626175.2_cs10_genomic.fna_nospace.fasta"
-#GENOME="../repeatmodeler/ERBxHO40_23_hirisehap1.masked"
-#GNAME="ERBxHO40_23_hirisehap1a"
-#GENOME="../repeatmodeler/CBR_1.asm.bp.hap1.p_ctg.gfa.fa.masked"
-#GNAME="CBR_hap1a"
-
-#GENOME="../RepeatModeler_softmask/GCA_003417725.2_ASM341772v2_genomic.fna.masked"
-#GENOME="../RepeatModeler_softmask/GCA_003417725.2_ASM341772v2_genomic.fna_nospace.fasta"
-#GNAME="Finola1"
-
 #GENOME="../repeatmodeler/GCA_000230575.5_ASM23057v5_genomic.fna.masked"
 GENOME="../repeatmodeler/GCA_000230575.5_ASM23057v5_genomic.fna_nospace.fasta"
 GNAME="Purple_Kush1"
-
-#RNASEQ_bam="../CBDRx_hisat2/bams/SRR10600876_sorted.bam,../CBDRx_hisat2/bams/SRR10600883_sorted.bam,../CBDRx_hisat2/bams/SRR10600884_sorted.bam,../CBDRx_hisat2/bams/SRR10600886_sorted.bam,../CBDRx_hisat2/bams/SRR10600901_sorted.bam,../CBDRx_hisat2/bams/SRR10600927_sorted.bam,../CBDRx_hisat2/bams/SRR10600944_sorted.bam"
-#RNASEQ_bam="../RNA_evidence/bams/ERVxH040-21-earlyflower_sorted.bam,../RNA_evidence/bams/ERVxH040-21-foliage12-12light-reextract_sorted.bam,../RNA_evidence/bams/ERVxH040-21-foliage_sorted.bam,../RNA_evidence/bams/ERVxH040-21-lateflower-reextract_sorted.bam,../RNA_evidence/bams/ERVxH040-21-roots_sorted.bam,../RNA_evidence/bams/ERVxH040-21-shoottips_sorted.bam"
 
 RNASEQ_bam="../RNA_evidence/bams/SRR10600876_sorted.bam,../RNA_evidence/bams/SRR10600883_sorted.bam,../RNA_evidence/bams/SRR10600884_sorted.bam,../RNA_evidence/bams/SRR10600886_sorted.bam,../RNA_evidence/bams/SRR10600901_sorted.bam,../RNA_evidence/bams/SRR10600927_sorted.bam,../RNA_evidence/bams/SRR10600944_sorted.bam"
 
