47b4e5ac3404052f85f28212d1bc88558618c6fc
Brian Knaus <briank.lists@gmail.com>
2023-11-08 12:14:27 -0800

    Update run_braker2.sh

--- SGE_scripts/run_braker2.sh
+++ SGE_scripts/run_braker2.sh
@@ -27,38 +27,9 @@ BRAKER="~/gits/BRAKER/scripts/braker.pl"
 braker.pl -version
 
 # Ideally a "soft masked" genome.
-#GENOME="../repeatmodeler/GCF_000146045.2_R64_genomic.fna.masked"
-#GENOME="~/Vining_Lab_nfs3/GENOMES/hemp/CBDRx/GCF_900626175/RepeatModeler/GCF_900626175.2_cs10_genomic.fna.masked"
-#GENOME="../CBDRx_masked_nospace.fna"
-#GENOME="GCF_900626175.2_cs10_genomic.fna_nospace.fasta"
-#GENOME="../CBDRx_braker3/GCF_900626175.2_cs10_genomic.fna_nospace.fasta"
-#GENOME="../braker_example/genome.fa"
-#GENOME="../repeatmodeler/ERBxHO40_23_hirisehap1.masked"
-#GNAME="ERBxHO40_23_hirisehap1b"
-#GENOME="../repeatmodeler/CBR_1.asm.bp.hap1.p_ctg.gfa.fa.masked"
-#GNAME="CBR_hap1b"
-
-# GENOME="../RepeatModeler_softmask/GCA_003417725.2_ASM341772v2_genomic.fna.masked"
-#GENOME="../RepeatModeler_softmask/GCA_003417725.2_ASM341772v2_genomic.fna_nospace.fasta"
-#GNAME="Finola2"
-
-#GENOME="../repeatmodeler/GCA_000230575.5_ASM23057v5_genomic.fna.masked"
 GENOME="../repeatmodeler/GCA_000230575.5_ASM23057v5_genomic.fna_nospace.fasta"
 GNAME="Purple_Kush2"
 
-# HINTS="../braker_rna/braker/braker.gtf" # Includes unsupported features (e.g., start_codon, CDS, gene, transcript).
-#HINTS="../braker_rna/braker/hintsfile.gff"
-#HINTS="../prothint/prothint_augustus.gff"
-#HINTS="../prothint/prothint_augustus.gff"
-#HINTS="/nfs4/HORT/Vining_Lab/Users/knausb/CBDRx_braker2/prothint/prothint_augustus.gff"
-#HINTS="braker2/prothint_augustus.gff"
-
-# ORTHODB="../../OrthoDB/proteins_plants.fasta"
-
-# PROT_SEQ="../braker_example/orthodb_small.fa"
-# PROT_SEQ="../OrthoDB/proteins_plants_CBDRx.fasta"
-# PROT_SEQ="~/Vining_Lab_nfs4/Users/knausb/OrthoDB/proteins_plants.fasta"
-# PROT_SEQ="~/Vining_Lab_nfs4/Users/knausb/OrthoDB/proteins_plants_CBDRx.fasta"
 # Does not like tilda expansion.
 PROT_SEQ="/home/bpp/knausb/Vining_Lab_nfs4/Users/knausb/OrthoDB/proteins_plants_CBDRx.fasta"
 
