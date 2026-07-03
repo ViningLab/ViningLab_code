93d664587d2b432c19b62f2f0ea9f469fc64f65b
Brian Knaus <briank.lists@gmail.com>
2026-07-03 15:00:13 -0700

    Create sbatch_deepvar.sh

---
+++ Slurm_gpu/sbatch_deepvar.sh
@@ -0,0 +1,161 @@
+#!/usr/bin/env bash
+
+# Submit job as follows.
+# sbatch sbatch_deepvar.sh  
+# squeue
+# scontrol hold job_id
+
+# salloc --account=boris --partition=boris --gpus-per-node=1 --mem=4000M
+
+
+# nvidia-smi
+# watch -d -n 0.5 nvidia-smi
+# squeue -o"%.7i %.9P %.8j %.8u %.2t %.10M %.6D %C" | less -S
+# sinfo -o "%n %e %m %a %c %C" | less
+
+# https://docs.alliancecan.ca/wiki/Using_GPUs_with_Slurm
+# https://numba.pydata.org/numba-doc/dev/user/5minguide.html
+
+#SBATCH --job-name=dpv_gp0
+# SBATCH --job-name=dpv_gp1
+#SBATCH --error=stdeo/deepv_%A_%a.err
+#SBATCH --output=stdeo/deepv_%A_%a.out
+#SBATCH --account=boris
+# SBATCH --account=boris,green
+#SBATCH --partition=boris
+# SBATCH --partition=all.q,boris,green
+#SBATCH --exclude=aspen9
+#SBATCH --ntasks=1
+# SBATCH --cpus-per-task=1
+# SBATCH --cpus-per-task=6
+# SBATCH --cpus-per-task=12
+# SBATCH --cpus-per-task=24
+#SBATCH --cpus-per-task=64 # Boris seems to top out at 56
+# SBATCH --cpus-per-task=256
+# Boris has 512 cores
+#SBATCH --gpus-per-node=1
+# SBATCH --mem=16000M
+# SBATCH --mem=32000M
+#SBATCH --mem=48000M
+# SBATCH --array=0-0%1
+# SBATCH --array=1-10%1
+# SBATCH --array=7-10%1
+# SBATCH --array=11-20%1
+# SBATCH --array=21-50%1
+
+
+i=${SLURM_ARRAY_TASK_ID}
+#CMD="This is array task ${SLURM_ARRAY_TASK_ID}"
+
+HOST=$(hostname)
+echo "Running on host: $HOST"
+
+
+# Input file containing sample name and FASTQ locations.
+FILE=( `cat "samples.txt" `)
+IFS=';' read -a arr <<< "${FILE[$i]}"
+echo "${arr[1]}"
+
+##### ##### ##### ##### #####
+
+# Seconds since the shell was spawned, reset to zero here.
+SECONDS=0
+
+# https://docs.sylabs.io/guides/latest/user-guide/index.html
+# https://docs.sylabs.io/guides/3.5/user-guide/gpu.html
+
+# https://docs.nvidia.com/clara/parabricks/latest/documentation/tooldocs/man_deepvariant.html
+# https://docs.nvidia.com/clara/parabricks/4.0.1/documentation/tooldocs/man_deepvariant.html#man-deepvariant
+
+# https://docs.nvidia.com/clara/parabricks/latest/tutorials/dvtraining.html
+
+# https://github.com/google/deepvariant/tree/r1.6.1
+
+
+MY_SIF="/nfs4/HORT/Vining_Lab/Users/knausb/singularity_images/clara-parabricks_4.6.0-1.sif"
+
+# MY_SAMPLE="OR-1_S1"
+# MY_SAMPLE="OR-2_S2"
+# MY_SAMPLE="OR-3_S3"
+# MY_SAMPLE="OR-4_S4"
+
+BASE_DIR="/nfs4/HORT/Vining_Lab/Users/knausb/fiber_hemp/"
+
+#REF_DIR="~/Vining_Lab_nfs4/GENOMES/hemp/public_databases/NCBI/Pink_pepper/"
+#MY_REF="GCF_029168945.1_ASM2916894v1_genomic.fna.gz"
+REF_DIR="~/Vining_Lab_nfs4/GENOMES/hemp/public_databases/NCBI/Pink_pepper/gatk_dict/"
+MY_REF="GCF_029168945.1_ASM2916894v1_genomic.fna"
+
+BAM_DIR="/home/bpp/knausb/Vining_Lab_nfs7/PROJECTS/hemp/fiber/growing_year_2024_variants/MarkDuplicates/bams/"
+#MY_BAM="OR-1_S1_dupmrk.bam"
+MY_BAM=$MY_SAMPLE"_dupmrk.bam"
+
+
+# OUT_DIR="/nfs4/HORT/Vining_Lab/Users/knausb/fiber_hemp/vcfs"
+GVCF_DIR="/nfs4/HORT/Vining_Lab/Users/knausb/fiber_hemp/gvcfs"
+GVCF_DIR="${BASE_DIR}gvcfs/"
+MY_GVCF="${MY_SAMPLE}.g.vcf.gz"
+
+# SINGULARITYENV_CUDA_VISIBLE_DEVICES=0 singularity shell --nv ../singularity_images/clara-parabricks_4.6.0-1.sif 
+
+# singularity shell --nv --bind /home/bpp/knausb/Vining_Lab_nfs7/PROJECTS/hemp/fiber/growing_year_2024_variants/MarkDuplicates/bams/:/bamdir ../singularity_images/clara-parabricks_4.6.0-1.sif
+
+#
+CMD="export SINGULARITYENV_CUDA_VISIBLE_DEVICES=0"
+#CMD="export SINGULARITYENV_CUDA_VISIBLE_DEVICES=1"
+
+
+echo $CMD
+eval $CMD
+echo
+
+
+# --nv   enable experimental Nvidia support
+#  -W, --workdir string         working directory to be used for /tmp,
+#                               /var/tmp and $HOME (if -c/--contain was
+#                               also used)
+
+
+#CMD="SINGULARITYENV_CUDA_VISIBLE_DEVICES=0 singularity run --nv \
+CMD="singularity run --nv \
+  --bind ${REF_DIR}:/refdir \
+  --bind ${BAM_DIR}:/bamdir \
+  --bind ${GVCF_DIR}:/gvcfdir \
+  --bind /scratch:/workdir \
+  --bind $(pwd):/outputdir \
+  --workdir /workdir \
+  $MY_SIF \
+  pbrun deepvariant \
+  --mode shortread \
+  --ref /refdir/${MY_REF} \
+  --in-bam /bamdir/${MY_BAM} \
+  --out-variants /gvcfdir/${MY_GVCF} \
+  --gvcf \
+  --num-cpu-threads-per-stream 6 \
+  --num-gpus 1"
+
+
+
+#  --out-variants /gvcfdir/${MY_GVCF} \
+# \
+#  --interval NC_083601.1 Cannabis sativa cultivar Pink pepper isolate KNU-18-1 chromosome 1, ASM2916894v1, whole genome shotgun sequence:1-1000000"
+
+# --interval NC_083601.1:1-10000000"
+#  --gvcf /gvcfdir/${MY_GVCF}"
+
+
+echo $CMD
+# 
+eval $CMD
+
+
+# Report elapsed time.
+echo
+ELAPSED="Elapsed: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
+echo $ELAPSED
+
+
+##### ##### ##### ##### #####
+# EOF.
+
+
